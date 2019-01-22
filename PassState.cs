8aa227559d58ab59cf371b28a2616d1116e49e1f
p6k0 <42266894+p6k0@users.noreply.github.com>
2019-01-22 08:16:55 +0300

    Update PassState.cs

--- PassState.cs
+++ PassState.cs
@@ -4,54 +4,101 @@ namespace TurnCtrl
 {
     class PassState
     {
-        /// <summary>
-        /// Состояние проходов на вход или выход;
-        /// </summary>
-        readonly Travel
-            InTravel,
-            OutTravel;
-
 
     }
 
-    abstract class PassStateParam
+    abstract class SensorState
     {
+
         /// <summary>
         /// Время последнего изменения
         /// </summary>
-        readonly DateTime Changed;
+        public int Changed;
+        public bool State
+        {
+            get => _State;
+            set
+            {
+                if (_State != value)
+                {
+                    (_State ? CurrentOn : CurrentOff).Inc((ushort)(GetUnixNow() - Changed));
+                    _State = value;
+                    Changed = GetUnixNow();
+                }
+            }
+        }
+        private bool _State;
+
+        public CountTimer CurrentOn, CurrentOff;
+        public AveragePrediction AvgOn, AvgOff;
+
+        private readonly static DateTime UnixEpoch = new DateTime(1970, 1, 1);
+        /// <summary>
+        /// Возвращает метку времени UNIX
+        /// </summary>
+        /// <returns></returns>
+        private static int GetUnixNow()
+        {
+            return (int)DateTime.UtcNow.Subtract(UnixEpoch).TotalSeconds;
+        }
     }
 
-    class Travel : PassStateParam
+    class AveragePrediction
     {
-        /// <summary>
-        /// Число проходов
-        /// </summary>
-        readonly int Count;
-        private
-        /// <summary>
-        /// Открыт ли проход сейчас
-        /// </summary>
-        readonly bool CurrentlyOpen;
+        public ushort Count { get; private set; }
+        public ushort Duration { get; private set; }
+        public AveragePrediction()
+        {
+            Count = 0;
+            Duration = 0;
+        }
+        public AveragePrediction(ushort Count, ushort Duration)
+        {
+            this.Count = Count;
+            this.Duration = Duration;
+        }
+        public void Update(ushort Count, ushort Duration)
+        {
+            this.Count = Count;
+            this.Duration = Duration;
+        }
+
     }
 
-    class SensorState : PassStateParam
+    class CountTimer
     {
         /// <summary>
-        /// Состояние датчика
+        /// Число переключений
         /// </summary>
-        readonly bool State;
-    }
+        public ushort Count { get; private set; } = 0;
+        /// <summary>
+        /// Общая длительность нахождения в данном состоянии
+        /// </summary>
+        public ushort Duration { get; private set; } = 0;
 
-    class DoorState
-    {
-        readonly bool isSlider;
-        readonly SensorState[] Sensors = new SensorState[]{
-           new SensorState(),new SensorState(),new SensorState()
-        };
-        SensorState OpenIn { get => Sensors[0]; }
-        SensorState OpenOut { get => Sensors[1]; }
-        SensorState Closed { get => Sensors[2]; }
-        SensorState Damage { get => Sensors[2]; }
+        public CountTimer(ushort Count, ushort Duration)
+        {
+            this.Count = Count;
+            this.Duration = Duration;
+        }
+
+        /// <summary>
+        /// Увеличение счетчика 
+        /// </summary>
+        /// <param name="Duration">Ч исло секунд нахождения в состоянии</param>
+        public void Inc(ushort Duration)
+        {
+            Count++;
+            Duration += Duration;
+        }
+
+        /// <summary>
+        /// Сброс счетчика
+        /// </summary>
+        public void Reset()
+        {
+            Count = 0;
+            Duration = 0;
+        }
     }
 }
